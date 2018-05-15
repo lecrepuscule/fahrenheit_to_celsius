25052585febd29301c4abe0ad72c3956fd391a51
Yao Yuan <yaoyuan@thoughtworks.com>
2018-05-15 23:17:09 +0200

    added feature to convert from celsius to fahrenheit, but should be refactored further

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
 
 	loop{
 
-    	println!("Enter the temperature in celcius.");
+		println!("Enter the temperature.");
 
     	let mut f_temp = String::new();
     	io::stdin().read_line(&mut f_temp)
@@ -15,7 +15,22 @@ fn main() {
     		Err(_) => continue,
     	};
 
-    	println!("The temperature in celsius is: {}", f_to_c(f_temp));
+    	println!("Enter the unit, 'F' or 'C'.");
+
+		let mut unit = String::new();
+		io::stdin().read_line(&mut unit)
+			.expect("Failed to read line");
+
+		let unit = unit.trim();
+
+		if unit == "F" {
+			println!("The temperature in {} is: {}", unit, f_to_c(f_temp));
+		} else if unit == "C" {
+			println!("The temperature in {} is: {}", unit, c_to_f(f_temp));
+		} else {
+			continue
+		};
+
     	break;
 	};
 
@@ -24,4 +39,8 @@ fn main() {
 
 fn f_to_c(temperature: f64) -> f64 {
 	(temperature - 32.0) * 5.0 / 9.0
+}
+
+fn c_to_f(temperature: f64) -> f64 {
+	temperature * 9.0 / 5.0 + 32.0
 }
